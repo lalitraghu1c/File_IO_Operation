b611408557d970c8e73f41f9a8d4ed518fbe142d
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-12-10 16:19:18 +0530

    [Lalit] Add: JSON Serialization and De-Serialization

---
+++ FileIOOperation/JSONSerialization.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static FileIOOperation.JSONSerialization;
+
+namespace FileIOOperation
+{
+    public class JSONSerialization
+    {
+        public static void JsonDeSerialization()
+        {
+            string json = @"{
+                'Name' : 'Lalit',
+                'Description' : 'Welcome to Coding Bootcamp'
+            }";
+            BlogSites bsObj = JsonConvert.DeserializeObject<BlogSites>(json);
+            Console.WriteLine("Name = {0} \nDescription = {1}",bsObj.Name,bsObj.Description);
+        }
+        public static void JsonSerialization()
+        {
+            BlogSites bsObj = new BlogSites()
+            {
+                Name = "Lalit",
+                Description = "Welcome to Coding Bootcamp"
+            };
+            string jsonData = JsonConvert.SerializeObject(bsObj);
+            Console.WriteLine(jsonData);
+        }
+        public class BlogSites
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+    }
+}
--- FileIOOperation/Program.cs
+++ FileIOOperation/Program.cs
@@ -13,7 +13,7 @@ namespace FileIOOperation
             bool choice = true;
             while (choice)
             {
-                Console.WriteLine(" 1. Checking Whether the File Exists or Not\n 2. Read All Lines\n 3. Read All Text\n 4. Copying File\n 5. Deleting File\n 6. Read From StreamReader\n 7. Write Using StreamWriter\n 8. Binary Serialization\n 9. Binary De-Serialization");
+                Console.WriteLine(" 1. Checking Whether the File Exists or Not\n 2. Read All Lines\n 3. Read All Text\n 4. Copying File\n 5. Deleting File\n 6. Read From StreamReader\n 7. Write Using StreamWriter\n 8. Binary Serialization\n 9. Binary De-Serialization\n 10. Json Serialization\n 11. Json Serialization and De-Serialization");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -45,6 +45,12 @@ namespace FileIOOperation
                         break;
                     case 9:
                         BinarySerialization.Deserialization();
+                        break;
+                    case 10:
+                        JSONSerialization.JsonSerialization();
+                        break;
+                    case 11:
+                        JSONSerialization.JsonDeSerialization();
                         break;
                     default:
                         choice = false;
